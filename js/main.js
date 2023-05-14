9e387d223515b4186e78dfe844e3c38e7513ca82
GingShan <105120159+GingShan@users.noreply.github.com>
2023-05-14 21:53:40 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,74 +1,74 @@
 $(function () {
   $("#PlayBtn").click(() => {
-    $("#PlayBtn").fadeOut(800);
-    $("#PauseBtn").fadeIn(800);
+    $("#PlayBtn").fadeOut(500);
+    $("#PauseBtn").fadeIn(500);
   });
   $("#PauseBtn").click(() => {
-    $("#PlayBtn").fadeIn(800);
-    $("#PauseBtn").fadeOut(800);
+    $("#PlayBtn").fadeIn(500);
+    $("#PauseBtn").fadeOut(500);
   });
 
   $("#PlayBtn2").click(() => {
-    $("#PlayBtn2").fadeOut(800);
-    $("#PauseBtn2").fadeIn(800);
+    $("#PlayBtn2").fadeOut(500);
+    $("#PauseBtn2").fadeIn(500);
   });
   $("#PauseBtn2").click(() => {
-    $("#PlayBtn2").fadeIn(800);
-    $("#PauseBtn2").fadeOut(800);
+    $("#PlayBtn2").fadeIn(500);
+    $("#PauseBtn2").fadeOut(500);
   });
 
   $("#PlayBtn3").click(() => {
-    $("#PlayBtn3").fadeOut(800);
-    $("#PauseBtn3").fadeIn(800);
+    $("#PlayBtn3").fadeOut(500);
+    $("#PauseBtn3").fadeIn(500);
   });
   $("#PauseBtn3").click(() => {
-    $("#PlayBtn3").fadeIn(800);
-    $("#PauseBtn3").fadeOut(800);
+    $("#PlayBtn3").fadeIn(500);
+    $("#PauseBtn3").fadeOut(500);
   });
 
   $("#PlayBtn4").click(() => {
-    $("#PlayBtn4").fadeOut(800);
-    $("#PauseBtn4").fadeIn(800);
+    $("#PlayBtn4").fadeOut(500);
+    $("#PauseBtn4").fadeIn(500);
   });
   $("#PauseBtn4").click(() => {
-    $("#PlayBtn4").fadeIn(800);
-    $("#PauseBtn4").fadeOut(800);
+    $("#PlayBtn4").fadeIn(500);
+    $("#PauseBtn4").fadeOut(500);
   });
 
   $("#PlayBtn5").click(() => {
-    $("#PlayBtn5").fadeOut(800);
-    $("#PauseBtn5").fadeIn(800);
+    $("#PlayBtn5").fadeOut(500);
+    $("#PauseBtn5").fadeIn(500);
   });
   $("#PauseBtn5").click(() => {
-    $("#PlayBtn5").fadeIn(800);
-    $("#PauseBtn5").fadeOut(800);
+    $("#PlayBtn5").fadeIn(500);
+    $("#PauseBtn5").fadeOut(500);
   });
 
   $("#PlayBtn6").click(() => {
-    $("#PlayBtn6").fadeOut(800);
-    $("#PauseBtn6").fadeIn(800);
+    $("#PlayBtn6").fadeOut(500);
+    $("#PauseBtn6").fadeIn(500);
   });
   $("#PauseBtn6").click(() => {
-    $("#PlayBtn6").fadeIn(800);
-    $("#PauseBtn6").fadeOut(800);
+    $("#PlayBtn6").fadeIn(500);
+    $("#PauseBtn6").fadeOut(500);
   });
 
   $("#PlayBtn7").click(() => {
-    $("#PlayBtn7").fadeOut(800);
-    $("#PauseBtn7").fadeIn(800);
+    $("#PlayBtn7").fadeOut(500);
+    $("#PauseBtn7").fadeIn(500);
   });
   $("#PauseBtn7").click(() => {
-    $("#PlayBtn7").fadeIn(800);
-    $("#PauseBtn7").fadeOut(800);
+    $("#PlayBtn7").fadeIn(500);
+    $("#PauseBtn7").fadeOut(500);
   });
 
   $("#PlayBtn8").click(() => {
-    $("#PlayBtn8").fadeOut(800);
-    $("#PauseBtn8").fadeIn(800);
+    $("#PlayBtn8").fadeOut(500);
+    $("#PauseBtn8").fadeIn(500);
   });
   $("#PauseBtn8").click(() => {
-    $("#PlayBtn8").fadeIn(800);
-    $("#PauseBtn8").fadeOut(800);
+    $("#PlayBtn8").fadeIn(500);
+    $("#PauseBtn8").fadeOut(500);
   });
 })
 
@@ -77,6 +77,8 @@ $(function () {
 
 document.addEventListener("DOMContentLoaded", function () {
 
+  //var Music = document.getElementsByClassName("Music");
+  
   // 播放
   var btn_play_el = document.getElementById("PlayBtn");
   btn_play_el.addEventListener("click", function () {
@@ -109,6 +111,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var btn_play_el3 = document.getElementById("PlayBtn3");
   btn_play_el3.addEventListener("click", function () {
     let the_audio_el3 = document.getElementById("musicThree");
+    
     the_audio_el3.play();
   });
 
@@ -123,6 +126,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var btn_play_el4 = document.getElementById("PlayBtn4");
   btn_play_el4.addEventListener("click", function () {
     let the_audio_el4 = document.getElementById("musicFour");
+    
     the_audio_el4.play();
   });
 
@@ -137,6 +141,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var btn_play_el5 = document.getElementById("PlayBtn5");
   btn_play_el5.addEventListener("click", function () {
     let the_audio_el5 = document.getElementById("musicFive");
+    
     the_audio_el5.play();
   });
 
@@ -151,6 +156,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var btn_play_el6 = document.getElementById("PlayBtn6");
   btn_play_el6.addEventListener("click", function () {
     let the_audio_el6 = document.getElementById("musicSix");
+    
     the_audio_el6.play();
   });
 
@@ -165,6 +171,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var btn_play_el7 = document.getElementById("PlayBtn7");
   btn_play_el7.addEventListener("click", function () {
     let the_audio_el7 = document.getElementById("musicSeven");
+    
     the_audio_el7.play();
   });
 
@@ -179,6 +186,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var btn_play_el8 = document.getElementById("PlayBtn8");
   btn_play_el8.addEventListener("click", function () {
     let the_audio_el8 = document.getElementById("musicEight");
+    
     the_audio_el8.play();
   });
 
